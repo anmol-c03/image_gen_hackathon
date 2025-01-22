e6dab6bc205208ee3109b0ca166dd78327242775
Anmol chalise <anmolc650@gmail.com>
2025-01-22 19:16:54 +0545

    define_loss_fn

---
+++ src/Fine_tuning/loss.py
@@ -0,0 +1,21 @@
+'''
+TODO:
+1. Define Pattern loss
+2. Define style loss
+3. Define Geometric loss
+'''
+class loss_fn:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def pattern_loss(self):
+        pass
+
+    def geometric_loss(self):
+        pass
+
+    def style_loss(self):
+        pass
