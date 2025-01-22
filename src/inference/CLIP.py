3e8165c446413414b8fd83f454d6189d29f4beac
Anmol chalise <anmolc650@gmail.com>
2025-01-22 16:53:08 +0545

    generate_prompt

--- src/inference/CLIP.py
+++ src/inference/CLIP.py
@@ -38,3 +38,52 @@ def image_to_prompt(image, mode):
         return ci.interrogate_fast(image)
     elif mode == 'negative':
         return ci.interrogate_negative(image)
+
+
+def sanitize_for_filename(prompt: str, max_len: int) -> str:
+    name = "".join(c for c in prompt if (c.isalnum() or c in ",._-! "))
+    name = name.strip()[:(max_len-4)] # extra space for extension
+    return name
+
+
+def generate_prompt(folder_path,prompt_mode,output_mode,max_filename_len):
+    files = [f for f in os.listdir(folder_path) if f.endswith('.jpg') or f.endswith('.png')] if os.path.exists(folder_path) else []
+    prompts = []
+    for idx, file in enumerate(tqdm(files, desc='Generating prompts')):
+        if idx > 0 and idx % 100 == 0:
+            clear_output(wait=True)
+
+        image = Image.open(os.path.join(folder_path, file)).convert('RGB')
+        prompt = image_to_prompt(image, prompt_mode)
+        prompts.append(prompt)
+
+        print(prompt)
+        thumb = image.copy()
+        thumb.thumbnail([256, 256])
+        display(thumb)
+
+        if output_mode == 'rename':
+            name = sanitize_for_filename(prompt, max_filename_len)
+            ext = os.path.splitext(file)[1]
+            filename = name + ext
+            idx = 1
+            while os.path.exists(os.path.join(folder_path, filename)):
+                print(f'File {filename} already exists, trying {idx+1}...')
+                filename = f"{name}_{idx}{ext}"
+                idx += 1
+            os.rename(os.path.join(folder_path, file), os.path.join(folder_path, filename))
+
+    if len(prompts):
+        if output_mode == 'desc.csv':
+            csv_path = os.path.join(folder_path, 'desc.csv')
+            with open(csv_path, 'w', encoding='utf-8', newline='') as f:
+                w = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+                w.writerow(['image', 'prompt'])
+                for file, prompt in zip(files, prompts):
+                    w.writerow([file, prompt])
+
+            print(f"\n\n\n\nGenerated {len(prompts)} prompts and saved to {csv_path}, enjoy!")
+        else:
+            print(f"\n\n\n\nGenerated {len(prompts)} prompts and renamed your files, enjoy!")
+    else:
+        print(f"Sorry, I couldn't find any images in {folder_path}")
