dd2f97007ba60655391810d4799d36da47cfea7e
Anmol chalise <anmolc650@gmail.com>
2025-01-22 16:54:22 +0545

    stability_api_call

---
+++ src/inference/stability.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+import os 
+from PIL import Image
+import IPython
+import json
+import os
+from PIL import Image
+import requests
+import time
+
+
+import getpass
+# @markdown To get your API key visit https://platform.stability.ai/account/keys
+STABILITY_KEY = getpass.getpass('<your_api_key>')
+
+def send_generation_request(
+    host,
+    params,
+):
+    headers = {
+        "Accept": "image/*",
+        "Authorization": f"Bearer {STABILITY_KEY}"
+    }
+
+    # Encode parameters
+    files = {}
+    image = params.pop("image", None)
+    mask = params.pop("mask", None)
+    if image is not None and image != '':
+        files["image"] = open(image, 'rb')
+    if mask is not None and mask != '':
+        files["mask"] = open(mask, 'rb')
+    if len(files)==0:
+        files["none"] = ''
+
+    # Send request
+    print(f"Sending REST request to {host}...")
+    response = requests.post(
+        host,
+        headers=headers,
+        files=files,
+        data=params
+    )
+    if not response.ok:
+        raise Exception(f"HTTP {response.status_code}: {response.text}")
+
+    return response
+
