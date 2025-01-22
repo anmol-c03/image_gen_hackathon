743ee7cd6ecca2cc035a9c4bc89add859c6eb002
Anmol chalise <anmolc650@gmail.com>
2025-01-22 16:52:35 +0545

    mdoel_name and img_to_prmpt

--- src/inference/CLIP.py
+++ src/inference/CLIP.py
@@ -14,3 +14,27 @@ def setup():
     ]
     for cmd in install_cmds:
         print(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8'))
+
+
+caption_model_name = 'blip-large' #@param ["blip-base", "blip-large", "git-large-coco"]
+clip_model_name = 'ViT-L-14/openai' #@param ["ViT-L-14/openai", "ViT-H-14/laion2b_s32b_b79k"]
+
+
+
+config = Config()
+config.clip_model_name = clip_model_name
+config.caption_model_name = caption_model_name
+ci = Interrogator(config)
+
+def image_to_prompt(image, mode):
+    ci.config.chunk_size = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
+    ci.config.flavor_intermediate_count = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
+    image = image.convert('RGB')
+    if mode == 'best':
+        return ci.interrogate(image)
+    elif mode == 'classic':
+        return ci.interrogate_classic(image)
+    elif mode == 'fast':
+        return ci.interrogate_fast(image)
+    elif mode == 'negative':
+        return ci.interrogate_negative(image)
